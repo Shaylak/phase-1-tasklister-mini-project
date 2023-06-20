b409bc37bbef19b18fcf799a203c85a86f83bcbb
Shaylak <shayyylaaaking@gmail.com>
2023-06-20 16:51:58 -0700

    tasklistner completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   addingEventListeners()
 });
+
+function addingEventListeners () {
+   document.getElementById("create-task-form").addEventListener("submit", handleFormSubmit)
+
+}
+
+function handleFormSubmit(e) {
+   e.preventDefault()
+   console.log(e)
+   const task = e.target[0].value
+   const priorityLevel = parseInt(e.target.priority.value)
+
+   //console.log(priorityLevel)
+
+   displayTask(task, priorityLevel)
+   
+}
+ 
+function displayTask(task, priorityLevel) {
+   console.log(task, priorityLevel)
+   
+    const taskUl = document.getElementById("tasks")
+    const taskLi = document.createElement("li")
+    const deleteBtn = document.createElement("button")
+
+    deleteBtn.textContent = "x"
+    deleteBtn.addEventListener("click", deleteTask)
+
+    taskLi.textContent = task + " "
+    taskLi.style.color = getPriorityColor(priorityLevel)
+    taskLi.appendChild(deleteBtn)
+    taskUl.appendChild(taskLi)
+}
+
+function deleteTask(e) {
+  console.log(e)
+  e.target.parentNode.remove()
+
+}
+
+function getPriorityColor(priorityLevel) {
+   if (priorityLevel === 1) {
+       return "red"
+   } else if (priorityLevel === 2) {
+        return "blue"
+   } else {
+       return "green"
+   }
+
+}  
